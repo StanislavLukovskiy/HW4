f6a70894ccd0e97e51fd92b6f300ee00144f68cd
stas <point.blank.point@bk.ru>
2023-04-06 22:22:05 +0300

    exz1 done

--- exz1/Program.cs
+++ exz1/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int ReadInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int number1 = ReadInt("Введите первое число ");
+int number2 = ReadInt("Введите второе число ");
+for(int i = number1; i <= number1; i++)
+{
+System.Console.Write(Math.Pow(i,number2) + " ");
+}
